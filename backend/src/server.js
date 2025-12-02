6068e57bdfac8cd547980d038193265ee2ba63cb
keerthivasan <keerthivasanclgdesign@gmail.com>
2025-12-02 11:55:52 +0800

    first commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,31 +1,37 @@
 import express from "express";
-import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import path from "path";
+import cors from "cors";
 
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
+import { ENV } from "./lib/env.js";
+import { app, server } from "./lib/socket.js";
 
+const __dirname = path.resolve();
 
-dotenv.config();
+const PORT = ENV.PORT || 3000;
 
-const app = express();
-const __dirname =path.resolve();
+// app.use(express.json({ limit: "5mb" })); // req.body
+app.use(express.json({ limit: "20mb" }));
+app.use(express.urlencoded({ extended: true, limit: "20mb" }));
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(cookieParser());
 
-const PORT = process.env.PORT || 3000;
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoutes)
+// make ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-//make ready to deployment
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
-
-    app.get("*", (_,res)=> {
-        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
-    })
-
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
 }
-app.listen(PORT, () => console.log("server running on port: " +PORT));
 
-
-
+server.listen(PORT, () => {
+  console.log("Server running on port: " + PORT);
+  connectDB();
+});
